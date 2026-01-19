182fad0964c957546fd2919bbd9ec73dc1a4a15e
naresh <Naresh@gmail.com>
2026-01-19 19:58:13 +0530

    vpc module modification:tags

--- terraform-vpc-module/variables.tf
+++ terraform-vpc-module/variables.tf
@@ -23,19 +23,19 @@ variable "environment" {
 }
 variable "public_subnet" {
    type        = list(string)
-   default = ["10.0.1.0/24","10.0.2.0/24"]
+   
 }
 variable "private_subnet" {
     type        = list(string)
-    default = ["10.0.11.0/24","10.0.12.0/24"]
+    
 }
 variable "db_subnet" {
      type        = list(string)
-     default = ["10.0.21.0/24","10.0.22.0/24"]
+     
 }
 variable "availability_zones" {
      type        = list(string)
-     default = ["us-east-1a","us-east-1b"]
+     
 }
 
 variable "is_vpc_peer_required" {
